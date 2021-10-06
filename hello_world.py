0157a88d092ffd33a7195fe706024a3415841af7
Krista Poutanen <krista.poutanen@live.fi>
2021-10-06 08:47:13 +0300

    {Tehty tuloste}

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+"""
+COMP.CS.100 Ohjelmointi 1
+Tekijä: Krista Poutanen
+Opiskelijanumero: K440484
+Sähköposti: krista.poutanen@tuni.fi
+"""
+
+
+def main():
+print("Hello World!")
+
+if __name__ == "__main__":
+    main()
